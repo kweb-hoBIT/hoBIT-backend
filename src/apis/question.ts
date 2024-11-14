99b5287afc74d312954d7df24d4fa7bffe7d5d0b
KangMinSun <chim38713@gmail.com>
2024-11-14 19:45:33 +0900

    feat: create insertFaqLog

--- src/apis/question.ts
+++ src/apis/question.ts
@@ -8,6 +8,7 @@ import {
 import { fetchAllFaqs } from "../db_interface/faq";
 import { PoolConnection } from "mysql2/promise";
 import { Pool } from "../../config/connectDB";
+import { insertFaqLog } from "../db_interface/faqLog";
 
 export async function allFaqs(res: Response<AllFaqsResponse | ErrorResponse>) {
   const conn: PoolConnection = await Pool.getConnection();
@@ -36,6 +37,19 @@ export async function question(
     // 유사도 기반 질문 탐색 알고리즘 수행
 
     // res.send({ answer });
+
+     // FAQ 로그 저장
+     const newFaqLog = {
+      user_id: null,
+      faq_id: null,
+      prev_faq: "",
+      new_faq: question,
+      action_type: "Question Submitted",
+    };
+
+    await insertFaqLog(conn, newFaqLog);
+
+    res.json({ question })
   } catch (error: any) {
     console.error(error.message);
     res.status(500).json({ error: "get_answer 함수 호출 실패" });
