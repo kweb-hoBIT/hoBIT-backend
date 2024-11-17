aa350a50ae3d0ec12a6fdd4437ef9f4a8164e887
Chan Heo <71842796+hchannni@users.noreply.github.com>
2024-11-17 18:54:33 +0900

    feat: internal external error split

--- src/server/run.ts
+++ src/server/run.ts
@@ -4,6 +4,7 @@ import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from '../../swaggerConfig';
 import { router } from '../router/index';
 import { errorHandler } from '../middleware/error_handler';
+import { NotFoundError } from '../types';
 
 const PORT = 4000;
 const API_V0 = '/api/v0';
@@ -19,6 +20,11 @@ export async function runServer() {
 
   app.use(express.json());
   app.use(API_V0, router);
+
+  // 처리되지 않은 리소스에 대한 NotFoundError 추가
+  app.use((req, _res, next) => {
+    next(new NotFoundError(`The requested resource ${req.path} was not found`));
+  });
   app.use(errorHandler);
 
   app.listen(PORT, () => {
