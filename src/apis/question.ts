3569746f25af661b91ced7cf99ea1300259c14c1
Heochan <heochan01@korea.ac.kr>
2024-11-19 15:39:30 +0900

    feat: add question_alter handler in /apis/question.ts

--- src/apis/question.ts
+++ src/apis/question.ts
@@ -3,8 +3,9 @@ import { PoolConnection } from 'mysql2/promise';
 
 import { Pool } from '../../config/connectDB';
 import TQuestionLog from '../models/QuestionLog';
-import { isEnglish } from '../lib/lang_tools';
-import { insertQuestionLog } from '../db_interface';
+import TFAQ from '../models/FAQ';
+import { isEnglish, tokenizeEnglish, tokenizeKorean } from '../lib/lang_tools';
+import { fetchAllFaqs, insertQuestionLog } from '../db_interface';
 import {
   ErrorResponse,
   NluError,
@@ -57,4 +58,63 @@ export const question = async (
   } finally {
     conn.release();
   }
+};
+
+export const question_alter = async (
+  req: Request<{}, {}, QuestionRequest>,
+  res: Response<Array<TFAQ> | ErrorResponse>
+) => {
+  const { question } = req.body;
+  if (!question) {
+    throw new ValidationError("invalid parameter, 'question' required");
+  }
+
+  const conn: PoolConnection = await Pool.getConnection();
+
+  try {
+    const faqs: TFAQ[] = await fetchAllFaqs(conn);
+
+    const tokens = isEnglish(question)
+      ? tokenizeEnglish(question)
+      : tokenizeKorean(question);
+
+    const faqWeights = calculateFaqWeights(
+      faqs,
+      tokens,
+      isEnglish(question) ? 'question_en' : 'question_ko'
+    );
+
+    const filteredFaqs: Array<TFAQ> = faqs
+      .filter((faq) => faqWeights[faq.id]! > 0)
+      .sort((a, b) => faqWeights[b.id]! - faqWeights[a.id]!)
+      .slice(0, 5);
+
+    res.json(filteredFaqs);
+  } finally {
+    conn.release();
+  }
+};
+
+const calculateFaqWeights = (
+  faqs: TFAQ[],
+  tokens: string[],
+  field: 'question_en' | 'question_ko'
+): Record<number, number> => {
+  const faqWeights: Record<number, number> = {};
+
+  faqs.forEach((faq) => {
+    let weight = 0;
+
+    tokens.forEach((token) => {
+      const occurrences = (
+        faq[field].match(new RegExp(`\\b${token}\\b`, 'gi')) || []
+      ).length;
+
+      weight += occurrences;
+    });
+
+    faqWeights[faq.id] = weight;
+  });
+
+  return faqWeights;
 };
