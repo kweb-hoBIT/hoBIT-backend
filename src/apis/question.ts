83e7bb72297dba3d313382ba7ea31d6cd5d430a7
Heochan <heochan01@korea.ac.kr>
2024-11-22 14:21:44 +0900

    refactor: add error handling - 'question' & 'question_after' handler

--- src/apis/question.ts
+++ src/apis/question.ts
@@ -7,106 +7,110 @@ import TFAQ from '../models/FAQ';
 import { isEnglish, tokenizeEnglish, tokenizeKorean } from '../lib/lang_tools';
 import { calculateFaqWeights } from '../lib/qna_tools';
 import {
-	fetchAllFaqs,
-	insertQuestionLog,
-	fetchFaqByFaqIds,
+  fetchAllFaqs,
+  insertQuestionLog,
+  fetchFaqByFaqIds,
 } from '../db_interface';
 import {
-	ErrorResponse,
-	NluError,
-	NluRequest,
-	NluResponse,
-	QuestionAfterRequest,
-	QuestionAfterResponse,
-	QuestionRequest,
-	QuestionResponse,
-	ValidationError,
+  ErrorResponse,
+  NluError,
+  NluRequest,
+  NluResponse,
+  QuestionAfterRequest,
+  QuestionAfterResponse,
+  QuestionRequest,
+  QuestionResponse,
+  ValidationError,
 } from '../types';
 import { fetchNlu } from '../internal/api';
 
 export const question = async (
-	req: Request<{}, {}, QuestionRequest>,
-	res: Response<QuestionResponse | ErrorResponse>
+  req: Request<{}, {}, QuestionRequest>,
+  res: Response<QuestionResponse | ErrorResponse>
 ) => {
-	const { question, language } = req.body;
-	if (!question) {
-		throw new ValidationError("invalid parameter, 'question' required");
-	}
+  const { question, language } = req.body;
+  if (!question) {
+    throw new ValidationError("invalid parameter, 'question' required");
+  } else if (!language) {
+    throw new ValidationError("invalid parameter, 'language' required");
+  }
 
-	const conn: PoolConnection = await Pool.getConnection();
+  const conn: PoolConnection = await Pool.getConnection();
 
-	try {
-		let nluParams: NluRequest = {
-			sender: 'hobit-backend',
-			message: question,
-		};
+  try {
+    let nluParams: NluRequest = {
+      sender: 'hobit-backend',
+      message: question,
+    };
 
-		const nlpResp: NluResponse = await fetchNlu(nluParams);
+    const nlpResp: NluResponse = await fetchNlu(nluParams);
 
-		if (!nlpResp || !nlpResp[0]) {
-			throw new NluError('NLU 서버 요청 실패');
-		}
+    if (!nlpResp || !nlpResp[0]) {
+      throw new NluError('NLU 서버 요청 실패');
+    }
 
-		//TODO: clean code, ask nlu provider to provide response type
-		let all_faq_ids: Array<number> = [];
-		if ('text' in nlpResp[0] && nlpResp[1] && 'text' in nlpResp[1]) {
-			const faq_ids = [...nlpResp[1].text.matchAll(/#(\d+)/g)].map((match) =>
-				Number(match[1])
-			);
-			all_faq_ids = faq_ids;
-		} else if ('custom' in nlpResp[0]) {
-			all_faq_ids.push(nlpResp[0].custom?.faq_id);
-		}
+    //TODO: clean code, ask nlu provider to provide response type
+    let all_faq_ids: Array<number> = [];
+    if ('text' in nlpResp[0] && nlpResp[1] && 'text' in nlpResp[1]) {
+      const faq_ids = [...nlpResp[1].text.matchAll(/#(\d+)/g)].map((match) =>
+        Number(match[1])
+      );
+      all_faq_ids = faq_ids;
+    } else if ('custom' in nlpResp[0]) {
+      all_faq_ids.push(nlpResp[0].custom?.faq_id);
+    }
 
-		let faqs = await fetchFaqByFaqIds(conn, all_faq_ids);
-		res.status(200).json({ faqs: faqs });
+    let faqs = await fetchFaqByFaqIds(conn, all_faq_ids);
+    res.status(200).json({ faqs: faqs });
 
-		const questionLog: Omit<
-			TQuestionLog,
-			'id' | 'feedback_score' | 'feedback' | 'created_at'
-		> = {
-			faq_id: 1,
-			user_question: question,
-			language,
-		};
+    const questionLog: Omit<
+      TQuestionLog,
+      'id' | 'feedback_score' | 'feedback' | 'created_at'
+    > = {
+      faq_id: 1,
+      user_question: question,
+      language,
+    };
 
-		await insertQuestionLog(conn, questionLog);
-	} finally {
-		conn.release();
-	}
+    await insertQuestionLog(conn, questionLog);
+  } finally {
+    conn.release();
+  }
 };
 
 export const question_after = async (
-	req: Request<{}, {}, QuestionAfterRequest>,
-	res: Response<QuestionAfterResponse | ErrorResponse>
+  req: Request<{}, {}, QuestionAfterRequest>,
+  res: Response<QuestionAfterResponse | ErrorResponse>
 ) => {
-	const { question } = req.body;
-	if (!question) {
-		throw new ValidationError("invalid parameter, 'question' required");
-	}
+  const { question, language } = req.body;
+  if (!question) {
+    throw new ValidationError("invalid parameter, 'question' required");
+  } else if (!language) {
+    throw new ValidationError("invalid parameter, 'language' required");
+  }
 
-	const conn: PoolConnection = await Pool.getConnection();
+  const conn: PoolConnection = await Pool.getConnection();
 
-	try {
-		const faqs: TFAQ[] = await fetchAllFaqs(conn);
+  try {
+    const faqs: TFAQ[] = await fetchAllFaqs(conn);
 
-		const tokens = isEnglish(question)
-			? tokenizeEnglish(question)
-			: tokenizeKorean(question);
+    const tokens = isEnglish(question)
+      ? tokenizeEnglish(question)
+      : tokenizeKorean(question);
 
-		const faqWeights = calculateFaqWeights(
-			faqs,
-			tokens,
-			isEnglish(question) ? 'question_en' : 'question_ko'
-		);
+    const faqWeights = calculateFaqWeights(
+      faqs,
+      tokens,
+      isEnglish(question) ? 'question_en' : 'question_ko'
+    );
 
-		const filteredFaqs: Array<TFAQ> = faqs
-			.filter((faq) => faqWeights[faq.id]! > 0)
-			.sort((a, b) => faqWeights[b.id]! - faqWeights[a.id]!)
-			.slice(0, 5);
+    const filteredFaqs: Array<TFAQ> = faqs
+      .filter((faq) => faqWeights[faq.id]! > 0)
+      .sort((a, b) => faqWeights[b.id]! - faqWeights[a.id]!)
+      .slice(0, 5);
 
-		res.json({ answersList: filteredFaqs });
-	} finally {
-		conn.release();
-	}
+    res.json({ answersList: filteredFaqs });
+  } finally {
+    conn.release();
+  }
 };
