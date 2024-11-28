4523dc4fc22ecd979c58b897839070bad529f687
Ji Hwan <angeljihwan@gmail.com>
2024-11-28 21:26:44 +0900

    chore: off prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -42,7 +42,7 @@ export default [
     },
 
     rules: {
-      'prettier/prettier': 'error',
+      'prettier/prettier': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
