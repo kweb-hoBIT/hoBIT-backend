d66449af0a9ba98e24ffbeb8aea16dc8148f03d4
Heochan <heochan01@korea.ac.kr>
2025-01-01 23:21:10 +0900

    feat: user feedback models & types

---
+++ src/models/UserFeedback.ts
@@ -0,0 +1,13 @@
+import { QuestionLanguage } from "../types";
+import { UserFeedbackReason } from "../types/feedback";
+import TFAQ from "./FAQ";
+
+export type UserFeedback = {
+  id: number;
+  faq_id: TFAQ[] | null;
+  feedback_reason: UserFeedbackReason | null;
+  feedback_detail: string;
+  language: QuestionLanguage;
+  created_by: number | null;
+};
+
