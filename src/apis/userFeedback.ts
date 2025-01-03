5e3b17d1b5851f2dac502107045058b8c84e974e
Ji Hwan An <angeljihwan@gmail.com>
2025-01-03 16:57:33 +0900

    refactor: response type for feedback

--- src/apis/userFeedback.ts
+++ src/apis/userFeedback.ts
@@ -1,27 +1,31 @@
 import { Request, Response } from 'express';
 import { PoolConnection } from 'mysql2/promise';
-import { DirectUserFeedbacksRequest } from '../types/feedback';
+import {
+	DirectUserFeedbacksRequest,
+	DirectUserFeedbacksResponse,
+} from '../types/feedback';
 import { Pool } from '../../config/connectDB';
 import { insertUserFeedback } from '../db_interface/userFeedback';
+import { ErrorResponse } from '../types';
 
 export const directUserFeedbacks = async (
-  req: Request<DirectUserFeedbacksRequest>,
-  res: Response
+	req: Request<DirectUserFeedbacksRequest>,
+	res: Response<DirectUserFeedbacksResponse | ErrorResponse>
 ) => {
-  const { feedback_detail, language } = req.body;
-  const conn: PoolConnection = await Pool.getConnection();
+	const { feedback_detail, language } = req.body;
+	const conn: PoolConnection = await Pool.getConnection();
 
-  const feedback = {
-    feedback_detail,
-    language,
-    faq_id: null,
-    feedback_reason: null,
-  };
+	const feedback = {
+		feedback_detail,
+		language,
+		faq_id: null,
+		feedback_reason: null,
+	};
 
-  try {
-    await insertUserFeedback(conn, feedback);
-    res.json({ feedback: feedback });
-  } finally {
-    conn.release();
-  }
+	try {
+		await insertUserFeedback(conn, feedback);
+		res.json({ success: true });
+	} finally {
+		conn.release();
+	}
 };
