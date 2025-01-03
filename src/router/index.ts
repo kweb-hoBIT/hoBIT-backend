dd46579df5b2b04c128d4b8b8d4eb34ebc33a0f1
Chan Heo <71842796+hchannni@users.noreply.github.com>
2025-01-03 13:27:57 +0900

    Feat/ user feedback 기능 중 'Direct User Feedback pt. (#10)
    * feat: user feedback models & types
    
    * feat: DirectUserFeedbacksRequest types for api handler
    
    * feat: 'insertUserFeedback' db queries interface & 'directUserFeedbacks' handelr in new files
    
    * feat: /direct_user_feedback router endpoint
    
    * feat: create new 'user_feedbacks' table queries

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import { question, question_after } from '../apis/question';
 import { rateFaq } from '../apis/rate';
 import { allFaqs, allQuestions, topFaqs } from '../apis/faq';
 import { promiseHandler } from '../middleware/promise_handler';
+import { directUserFeedbacks } from '../apis/userFeedback';
 
 export const router = express.Router();
 
@@ -13,3 +14,4 @@ router.post('/question', promiseHandler(question));
 router.post('/question_after', promiseHandler(question_after));
 router.post('/rate', rateFaq);
 router.get('/top_faqs', promiseHandler(topFaqs));
+router.post('/direct_user_feedback', directUserFeedbacks);
