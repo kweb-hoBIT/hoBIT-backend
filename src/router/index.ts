d2af65d346695d46fd568724120379e668dc2f55
Kang Min-Sun <126390584+KangMinSun@users.noreply.github.com>
2025-01-10 03:25:17 +0900

    Prevent server crash on error handling

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,5 +13,5 @@ router.get('/all_questions', promiseHandler(allQuestions));
 router.get('/top_faqs', promiseHandler(topFaqs));
 router.post('/question', promiseHandler(question));
 router.post('/question_after', promiseHandler(question_after));
-router.post('/rate', rateFaq);
+router.post('/rate', promiseHandler(rateFaq));
 router.post('/direct_user_feedback', directUserFeedback);
