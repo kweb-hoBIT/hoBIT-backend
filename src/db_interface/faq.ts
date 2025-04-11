00735ff1a8c2dae1bf2779f33cf048275a69800a
Ji Hwan An <angeljihwan@gmail.com>
2025-04-11 22:22:06 +0900

    feat: category order

--- src/db_interface/faq.ts
+++ src/db_interface/faq.ts
@@ -3,100 +3,102 @@ import { DatabaseError, Question } from '../types';
 import TFAQ from '../models/FAQ';
 
 export async function fetchAllQuestions(
-  conn: PoolConnection
+	conn: PoolConnection
 ): Promise<Question[]> {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+	try {
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
 SELECT id, question_ko, question_en FROM faqs;
       `
-    );
+		);
 
-    const questions: Question[] = rows.map((row) => ({
-      faq_id: row['id'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-    }));
+		const questions: Question[] = rows.map((row) => ({
+			faq_id: row['id'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+		}));
 
-    return questions;
-  } catch (error: any) {
-    throw new DatabaseError('Failed to retrieve questions');
-  }
+		return questions;
+	} catch (error: any) {
+		throw new DatabaseError('Failed to retrieve questions');
+	}
 }
 
 export async function fetchAllFaqs(conn: PoolConnection) {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+	try {
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
 SELECT * FROM faqs;
 	`
-    );
+		);
 
-    const faqs: TFAQ[] = rows.map((row) => ({
-      id: row['id'],
-      maincategory_ko: row['maincategory_ko'],
-      maincategory_en: row['maincategory_en'],
-      subcategory_ko: row['subcategory_ko'],
-      subcategory_en: row['subcategory_en'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-      answer_ko: row['answer_ko'],
-      answer_en: row['answer_en'],
-      manager: row['manager'],
-      created_by: row['created_by'],
-      updated_by: row['updated_by'],
-    }));
+		const faqs: TFAQ[] = rows.map((row) => ({
+			id: row['id'],
+			maincategory_ko: row['maincategory_ko'],
+			maincategory_en: row['maincategory_en'],
+			subcategory_ko: row['subcategory_ko'],
+			subcategory_en: row['subcategory_en'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+			answer_ko: row['answer_ko'],
+			answer_en: row['answer_en'],
+			category_order: row['category_order'],
+			manager: row['manager'],
+			created_by: row['created_by'],
+			updated_by: row['updated_by'],
+		}));
 
-    return faqs;
-  } catch (error: any) {
-    throw new DatabaseError('Failed to retrieve all faqs');
-  }
+		return faqs;
+	} catch (error: any) {
+		throw new DatabaseError('Failed to retrieve all faqs');
+	}
 }
 
 export async function fetchFaqByFaqIds(
-  conn: PoolConnection,
-  faq_ids: number[]
+	conn: PoolConnection,
+	faq_ids: number[]
 ): Promise<TFAQ[]> {
-  try {
-    if (faq_ids.length === 0) return [];
+	try {
+		if (faq_ids.length === 0) return [];
 
-    const placeholders = faq_ids.map(() => '?').join(', ');
+		const placeholders = faq_ids.map(() => '?').join(', ');
 
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
 SELECT * FROM faqs
 WHERE id IN (${placeholders});
       `,
-      faq_ids
-    );
+			faq_ids
+		);
 
-    const faqs: TFAQ[] = rows.map((row) => ({
-      id: row['id'],
-      maincategory_ko: row['maincategory_ko'],
-      maincategory_en: row['maincategory_en'],
-      subcategory_ko: row['subcategory_ko'],
-      subcategory_en: row['subcategory_en'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-      answer_ko: row['answer_ko'],
-      answer_en: row['answer_en'],
-      manager: row['manager'],
-      created_by: row['created_by'],
-      updated_by: row['updated_by'],
-    }));
+		const faqs: TFAQ[] = rows.map((row) => ({
+			id: row['id'],
+			maincategory_ko: row['maincategory_ko'],
+			maincategory_en: row['maincategory_en'],
+			subcategory_ko: row['subcategory_ko'],
+			subcategory_en: row['subcategory_en'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+			answer_ko: row['answer_ko'],
+			answer_en: row['answer_en'],
+			category_order: row['category_order'],
+			manager: row['manager'],
+			created_by: row['created_by'],
+			updated_by: row['updated_by'],
+		}));
 
-    return faqs;
-  } catch (error: any) {
-    throw new DatabaseError(
-      `Failed to retrieve faq with ids: '${faq_ids.join(', ')}'`
-    );
-  }
+		return faqs;
+	} catch (error: any) {
+		throw new DatabaseError(
+			`Failed to retrieve faq with ids: '${faq_ids.join(', ')}'`
+		);
+	}
 }
 
 export async function fetchTopFaqs(conn: PoolConnection, limit: number) {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+	try {
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
       SELECT DISTINCT f.*
       FROM (
           SELECT faq_id, COUNT(*) AS count
@@ -109,98 +111,101 @@ export async function fetchTopFaqs(conn: PoolConnection, limit: number) {
       JOIN faqs f ON f.id = top_faqs.faq_id
       LIMIT ?;
       `,
-      [limit, limit]
-    );
+			[limit, limit]
+		);
 
-    const faqs: TFAQ[] = rows.map((row) => ({
-      id: row['id'],
-      maincategory_ko: row['maincategory_ko'],
-      maincategory_en: row['maincategory_en'],
-      subcategory_ko: row['subcategory_ko'],
-      subcategory_en: row['subcategory_en'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-      answer_ko: row['answer_ko'],
-      answer_en: row['answer_en'],
-      manager: row['manager'],
-      created_by: row['created_by'],
-      updated_by: row['updated_by'],
-    }));
+		const faqs: TFAQ[] = rows.map((row) => ({
+			id: row['id'],
+			maincategory_ko: row['maincategory_ko'],
+			maincategory_en: row['maincategory_en'],
+			subcategory_ko: row['subcategory_ko'],
+			subcategory_en: row['subcategory_en'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+			answer_ko: row['answer_ko'],
+			answer_en: row['answer_en'],
+			category_order: row['category_order'],
+			manager: row['manager'],
+			created_by: row['created_by'],
+			updated_by: row['updated_by'],
+		}));
 
-    return faqs;
-  } catch (error) {
-    throw new DatabaseError('Failed to retrieve top faqs');
-  }
+		return faqs;
+	} catch (error) {
+		throw new DatabaseError('Failed to retrieve top faqs');
+	}
 }
 
 export async function fetchFaqByQuestionKo(
-  conn: PoolConnection,
-  questionKo: string
+	conn: PoolConnection,
+	questionKo: string
 ): Promise<TFAQ[]> {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+	try {
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
 SELECT * FROM faqs
 WHERE question_ko = ?;
       `,
-      [questionKo]
-    );
+			[questionKo]
+		);
 
-    const faqs: TFAQ[] = rows.map((row) => ({
-      id: row['id'],
-      maincategory_ko: row['maincategory_ko'],
-      maincategory_en: row['maincategory_en'],
-      subcategory_ko: row['subcategory_ko'],
-      subcategory_en: row['subcategory_en'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-      answer_ko: row['answer_ko'],
-      answer_en: row['answer_en'],
-      manager: row['manager'],
-      created_by: row['created_by'],
-      updated_by: row['updated_by'],
-    }));
+		const faqs: TFAQ[] = rows.map((row) => ({
+			id: row['id'],
+			maincategory_ko: row['maincategory_ko'],
+			maincategory_en: row['maincategory_en'],
+			subcategory_ko: row['subcategory_ko'],
+			subcategory_en: row['subcategory_en'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+			answer_ko: row['answer_ko'],
+			answer_en: row['answer_en'],
+			category_order: row['category_order'],
+			manager: row['manager'],
+			created_by: row['created_by'],
+			updated_by: row['updated_by'],
+		}));
 
-    return faqs;
-  } catch (error: any) {
-    throw new DatabaseError(
-      `Failed to retrieve faq with corresponding '${questionKo}'`
-    );
-  }
+		return faqs;
+	} catch (error: any) {
+		throw new DatabaseError(
+			`Failed to retrieve faq with corresponding '${questionKo}'`
+		);
+	}
 }
 
 export async function fetchFaqByQuestionEn(
-  conn: PoolConnection,
-  questionEn: string
+	conn: PoolConnection,
+	questionEn: string
 ): Promise<TFAQ[]> {
-  try {
-    const [rows] = await conn.query<RowDataPacket[]>(
-      `
+	try {
+		const [rows] = await conn.query<RowDataPacket[]>(
+			`
 SELECT * FROM faqs
 WHERE question_en = ?;
       `,
-      [questionEn]
-    );
+			[questionEn]
+		);
 
-    const faqs: TFAQ[] = rows.map((row) => ({
-      id: row['id'],
-      maincategory_ko: row['maincategory_ko'],
-      maincategory_en: row['maincategory_en'],
-      subcategory_ko: row['subcategory_ko'],
-      subcategory_en: row['subcategory_en'],
-      question_ko: row['question_ko'],
-      question_en: row['question_en'],
-      answer_ko: row['answer_ko'],
-      answer_en: row['answer_en'],
-      manager: row['manager'],
-      created_by: row['created_by'],
-      updated_by: row['updated_by'],
-    }));
+		const faqs: TFAQ[] = rows.map((row) => ({
+			id: row['id'],
+			maincategory_ko: row['maincategory_ko'],
+			maincategory_en: row['maincategory_en'],
+			subcategory_ko: row['subcategory_ko'],
+			subcategory_en: row['subcategory_en'],
+			question_ko: row['question_ko'],
+			question_en: row['question_en'],
+			answer_ko: row['answer_ko'],
+			answer_en: row['answer_en'],
+			category_order: row['category_order'],
+			manager: row['manager'],
+			created_by: row['created_by'],
+			updated_by: row['updated_by'],
+		}));
 
-    return faqs;
-  } catch (error: any) {
-    throw new DatabaseError(
-      `Failed to retrieve faq with corresponding '${questionEn}'`
-    );
-  }
+		return faqs;
+	} catch (error: any) {
+		throw new DatabaseError(
+			`Failed to retrieve faq with corresponding '${questionEn}'`
+		);
+	}
 }
--- src/models/FAQ.ts
+++ src/models/FAQ.ts
@@ -1,25 +1,26 @@
 export type TFAQ = {
-  id: number;
-  maincategory_ko: string;
-  maincategory_en: string;
-  subcategory_ko: string;
-  subcategory_en: string;
-  question_ko: string;
-  question_en: string;
-  answer_ko: string;
-  answer_en: string;
-  manager: string;
-  created_by: number | null;
-  updated_by: number | null;
+	id: number;
+	maincategory_ko: string;
+	maincategory_en: string;
+	subcategory_ko: string;
+	subcategory_en: string;
+	question_ko: string;
+	question_en: string;
+	answer_ko: string;
+	answer_en: string;
+	manager: string;
+	category_order: number;
+	created_by: number | null;
+	updated_by: number | null;
 };
 
 export type TFaqAnswer = TFaqCard[];
 
 export type TFaqCard = {
-  answer: string;
-  url: string;
-  email: string;
-  phone: string;
+	answer: string;
+	url: string;
+	email: string;
+	phone: string;
 };
 
 export default TFAQ;
